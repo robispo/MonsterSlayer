875052eb0b8e510e3bb4bb7a0e8f00afb59c87b6
Rabel Obispo <rabelobispo@hotmail.com>
2019-12-18 15:22:53 -0500

    making some functions

--- app.js
+++ app.js
@@ -10,6 +10,38 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack: function() {
+      this.monsterHealth -= this.getDamage(3, 10);
+
+      if (this.monsterHealth <= 0) {
+        alert("You won!");
+        this.gameIsRunning = false;
+        return;
+      }
+
+      this.playerHealth -= this.getDamage(5, 12);
+
+      if (this.playerHealth <= 0) {
+        alert("You lose!");
+        this.gameIsRunning = false;
+      }
+    },
+    specialAttack: function() {},
+    heal: function() {},
+    giveUp: function() {},
+    getDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm("You won! New game?")) {
+          this.startGame();
+        }else {
+          this.gameIsRunning = false;
+        }
+        return;
+      }
     }
   }
 });
